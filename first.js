ebb517800be86d691fadfd0d1e42bb265d02f42b
TingaTejeswani <166682924+TingaTejeswani@users.noreply.github.com>
2025-04-25 17:55:27 +0530

    Update first.js

--- first.js
+++ first.js
@@ -23,6 +23,45 @@ const productSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('Product', productSchema);
+const Product = require('../models/Product');
+
+// Create product
+exports.createProduct = async (req, res) => {
+    const product = new Product(req.body);
+    await product.save();
+    res.status(201).json(product);
+};
+
+// Read all products
+exports.getAllProducts = async (req, res) => {
+    const products = await Product.find();
+    res.json(products);
+};
+
+// Read single product
+exports.getProductById = async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    res.json(product);
+};
+
+// Update product
+exports.updateProduct = async (req, res) => {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(product);
+};
+
+// Delete product
+exports.deleteProduct = async (req, res) => {
+    await Product.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Product deleted' });
+};
+
+// Optional: Search by name
+exports.searchProduct = async (req, res) => {
+    const name = req.query.name;
+    const result = await Product.find({ name: { $regex: name, $options: 'i' } });
+    res.json(result);
+};
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/productController');
@@ -36,4 +75,19 @@ router.delete('/products/:id', controller.deleteProduct);
 // Optional search
 router.get('/search', controller.searchProduct);
 
-module.exports = router;
+module.exports = router;const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const connectDB = require('./config/db');
+const productRoutes = require('./routes/productRoutes');
+
+const app = express();
+connectDB();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use('/api', productRoutes);
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(Server running on port ${PORT}));
